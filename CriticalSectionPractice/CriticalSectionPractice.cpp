3b9d53561edddc49de53b1afbcdf4bd633c5fd5a
zackjtl <chienting.lai@gmail.com>
2022-10-05 19:00:08 +0800

    修改comments

--- CriticalSectionPractice/CriticalSectionPractice.cpp
+++ CriticalSectionPractice/CriticalSectionPractice.cpp
@@ -15,6 +15,7 @@ int main()
     DWORD threadID[8];
     HANDLE threadHandle[8];
     
+    // 併發8個thread對同一個變數 (ClassA裡的_resource)做加減運算
     for (int i = 0; i < 8; ++i) {
         threadHandle[i] = CreateThread(0, 0, thread_proc, (LPVOID)i, 0, & threadID[i]);
     }
@@ -35,7 +36,7 @@ public:
     }
     void add(int value)
     {
-        Lock lock(*this); // 可以試看看把這行拿掉的結果有何不同
+        Lock lock(*this); // !! 可以試看看把這行拿掉的結果有何不同 !!
         
         if (value < 1)
             printf("resource = %d - %d = %d\n", ClassA::_resource, -value, ClassA::_resource + value);
@@ -60,20 +61,21 @@ public:
 // Static變數的功能和Global變數一樣
 //int ClassA::_resource = 0;
 
+// 產生ClassA的全域變數
 ClassA* g_Ca = new ClassA();
 
 DWORD WINAPI thread_proc(LPVOID lpParameter)
 {
-    // ClassA不再是全域變數，而是隨著thread產生的區域變數，因此可能同時有很多個
-
     //ClassA ca;
     time_t t;
 
     int threadIdx = (int)lpParameter;
-
-    //srand((unsigned)time(&t));
-
-    srand((int)lpParameter);
+                
+    // 在rand之前要呼叫srand，使得每次執行的亂數值都不一樣，如果不呼叫srand，每個thread的rand取得的值都一樣
+    // 用傳進來的index當作random seed，也可以如下行用時間當作seed，但由於CPU執行太快，每個thread的time()所得到的時間可能會都一樣，而用index可以確保不一樣
+    // srand((unsigned)time(&t));
+    
+    srand(threadIdx);
 
     int value = rand();
     int res = 0;
